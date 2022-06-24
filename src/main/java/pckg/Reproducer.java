dd66d444b40d4e5946d22f2813fb199dac7a1904
Johann Beleites <johann.beleites@sonarsource.com>
2022-06-24 15:18:57 +0200

    foo

---
+++ src/main/java/pckg/Reproducer.java
@@ -0,0 +1,30 @@
+package pckg;
+
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+class Foo {
+  public Date getPaginationDate() {
+    return null;
+  }
+}
+
+public class Reproducer {
+  public <T extends Foo> void foo(List<T> subList, List<T> completeListOfElements) {
+    final Optional<T> lastElement = subList
+      .stream()
+      .min(Comparator.comparing(T::getPaginationDate));
+    Date paginationLastTimestamp = null;
+    if (lastElement.isPresent()) {
+      final T lastFilteredElement = subList.get(subList.size() - 1);
+      final int indexOfLastElement = completeListOfElements.size() - 1;
+      if (completeListOfElements.indexOf(lastFilteredElement) < indexOfLastElement) {
+        paginationLastTimestamp = lastElement
+          .get()
+          .getPaginationDate();
+      }
+    }
+  }
+}
